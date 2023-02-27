29639a9fbee117c53d20792a01c148d29bd40162
jakegibson <jake@j3.io>
2023-02-27 14:01:30 -0700

    test dump

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,3 +3,10 @@ import Svelvet from './container/views/Svelvet.svelte';
 export default Svelvet;
 // types/types.ts contains types that are exported to the sure
 export * from './types/types';
+
+import { createNode } from './interactiveNodes/controllers/createNode.ts';
+
+import { getNodeById } from './nodes/controllers/util';
+
+export const createNode = createNode;
+export const getNodeById = ggetNodeById;
--- src/routes/testingplayground/index.svelte
+++ src/routes/testingplayground/index.svelte
@@ -1,7 +1,6 @@
-<script>
+<script type="ts">
   import Svelvet from "$lib/container/views/Svelvet.svelte";
-
-  const initialNodes = [
+  let initialNodes = [
     {
       id: 1,
       position: { x: 225, y: 10 },
@@ -68,7 +67,7 @@
     },
   ];
 
-  const initialEdges = [
+  let initialEdges = [
     { id: "e1-2", source: 1, target: 2, label: "edge label" },
     {
       id: "e2-3",
@@ -121,7 +120,7 @@
   let dropped = [];
   let status = "";
 
-  let dropped_in = "";
+  let dropped_in: boolean;
   let activeEvent = "";
   let originalX = "";
   let originalY = "";
@@ -135,6 +134,7 @@
   }
 
   function handleDragDrop(e) {
+    console.log('drop')
     e.preventDefault();
     var element_id = e.dataTransfer.getData("text");
     dropped = dropped.concat(element_id);
@@ -149,10 +149,54 @@
   }
 
   function handleDragEnd(e) {
-    if (dropped_in == false) {
-      status = "You let the " + e.target.getAttribute("id") + " go.";
+    console.log('dragend')
+    let pageX = parseInt(e.target.style.left) - 50;
+      let pageY = parseInt(e.target.style.top) - 50;
+    if (!drop_zone) {
+      drop_zone = document.getElementById("drop_zone");
     }
-    dropped_in = false;
+    if (
+        detectTouchEnd(
+          drop_zone.offsetLeft,
+          drop_zone.offsetTop,
+          pageX,
+          pageY,
+          drop_zone.offsetWidth,
+          drop_zone.offsetHeight
+        )
+      ) {
+        const store = stores["bob"]
+        console.log('store', stores)
+        // createNode(
+        //   store,
+        //    100,
+        //    100,
+        //    "bob"
+        // )
+        let newNodes = { "8": {
+      id: 8,
+      position: { x: 100, y: 100 },
+      data: { label: "Testeroni" },
+      width: 100,
+      height: 40,
+      bgColor: "white",
+      textColor: "black",
+    }}
+    let existingNodes = store.nodesStore
+        store.nodesStore.update((nodes) => {
+          console.log('nodes', nodes)
+          return {...newNodes , ...nodes}
+        });
+        dropped = dropped.concat(e.target.id);
+        e.target.style.position = "initial";
+        dropped_in = true;
+        status =
+          "You dropped " + e.target.getAttribute("id") + " into drop zone";
+      }
+    // if (dropped_in == false) {
+    //   status = "You let the " + e.target.getAttribute("id") + " go.";
+    // }
+    // dropped_in = false;
   }
 
   function handleTouchStart(e) {
@@ -174,6 +218,7 @@
   }
 
   function handleTouchEnd(e) {
+    console.log('touchend')
     e.preventDefault();
     if (activeEvent === "move") {
       let pageX = parseInt(e.target.style.left) - 50;
@@ -189,6 +234,7 @@
           drop_zone.offsetHeight
         )
       ) {
+        
         dropped = dropped.concat(e.target.id);
         e.target.style.position = "initial";
         dropped_in = true;
@@ -208,8 +254,168 @@
     if (y2 - y1 > h) return false;
     return true;
   }
-</script>
 
+
+  //asdfa
+  import { stores } from '$lib/store/models/store';
+
+  const pkStringGenerator = () => (Math.random() + 1).toString(36).substring(7);
+import type { StoreType, PotentialAnchorType } from '$lib/store/types/types';
+import { get } from 'svelte/store'; 
+import { Node } from '$lib/nodes/models/Node';
+import { Edge } from '$lib/edges/models/Edge';
+import { Anchor } from '$lib/edges/models/Anchor';
+
+import {
+  rightCb,
+  leftCb,
+  topCb,
+  bottomCb,
+} from '$lib/edges/controllers/anchorCbUser';
+import { createPotentialAnchor } from '$lib/store/controllers/util';
+import { dynamicCbCreator } from '$lib/edges/controllers/anchorCbDev';
+import { getAnchors } from '$lib/edges/controllers/util';
+function getPotentialAnchors(
+  store: StoreType,
+  filter?: { [key: string]: any }
+) {
+  let potentialAnchorsArr = Object.values(get(store.potentialAnchorsStore));
+  // filter the array for elements that match filter
+  if (filter !== undefined) {
+    potentialAnchorsArr = potentialAnchorsArr.filter((potentialAnchor) => {
+      for (let filterKey in filter) {
+        const filterValue = filter[filterKey];
+        if (
+          potentialAnchor[filterKey as keyof PotentialAnchorType] !==
+          filterValue
+        )
+          return false;
+      }
+      return true;
+    });
+  }
+  // return list of anchors
+  return potentialAnchorsArr;
+}
+
+/**
+ * Creates a new Node when user drags the Edge and releases mouse at a new spot. This functionality is needed for the "create new node by dragging" feature. See TemporaryEdge.createNode()
+ * This function has no output, but alters the store. New Node/Edge/Anchor objects are added to the store.
+ *
+ * @param store The Svelvet store containing the state of a Svelvet component
+ * @param sourceNodeId The id of the source Node
+ * @param targetNodeX The postion X where user releases the mouse, which should be the x-axis position for the new Node
+ * @param targetNodeY The postion Y where user releases the mouse, which should be the y-axis position for the new Node
+ * @param canvasId The canvasId of a Svelvet component
+ */
+function createNode(
+  store: StoreType,
+  targetNodeX: number,
+  targetNodeY: number,
+  canvasId: string
+) {
+  // create a new node
+  const targetNodeId = pkStringGenerator();
+
+  const node = new Node(
+    targetNodeId,
+    targetNodeX,
+    targetNodeY,
+    100, // width
+    100, // height
+    'white', // background color
+    {
+      label: 'new node',
+    },
+    canvasId,
+    'black', // borderColor
+    false, //  Node has an image
+    '', // src, not sure what this does
+    'black', // text color
+    0, // borderRadius
+    [],
+    '',
+    () => {}
+  );
+
+  // create new potential anchor, we do this because if we create
+  const potentialAnchorRight = createPotentialAnchor(
+    rightCb,
+    store,
+    targetNodeId,
+    canvasId
+  );
+  const potentialAnchorLeft = createPotentialAnchor(
+    leftCb,
+    store,
+    targetNodeId,
+    canvasId
+  );
+  const potentialAnchorTop = createPotentialAnchor(
+    topCb,
+    store,
+    targetNodeId,
+    canvasId
+  );
+  const potentialAnchorBottom = createPotentialAnchor(
+    bottomCb,
+    store,
+    targetNodeId,
+    canvasId
+  );
+
+  // create an edge
+  const edgeId = pkStringGenerator();
+  const newEdge = new Edge(
+    edgeId,
+    -1,
+    -1,
+    -1,
+    -1,
+    canvasId,
+    '',
+    'bezier',
+    'black',
+    'black',
+    'black',
+    false,
+    true,
+    false,
+    () => {},
+    ''
+  );
+
+ 
+
+
+  // put everything into the store
+  const { nodesStore, edgesStore, anchorsStore, potentialAnchorsStore } = store;
+
+  nodesStore.update((nodes) => {
+    nodes[targetNodeId] = node;
+    return { ...nodes };
+  });
+
+  potentialAnchorsStore.update((anchors) => {
+    anchors[potentialAnchorRight.id] = potentialAnchorRight;
+    anchors[potentialAnchorTop.id] = potentialAnchorTop;
+    anchors[potentialAnchorBottom.id] = potentialAnchorBottom;
+    anchors[potentialAnchorLeft.id] = potentialAnchorLeft;
+    return { ...anchors };
+  });
+  edgesStore.update((edges) => {
+    edges[edgeId] = newEdge;
+    return { ...edges };
+  });
+  // make sure to update positions of anchors and potential anchors. TODO: don't need to do this for the entire store
+  const anchors = getAnchors(store);
+  for (const anchor of anchors) anchor.callback();
+  const potAnchors = getPotentialAnchors(store);
+  for (const anchor of potAnchors) anchor.callback();
+}
+
+  ///asdfasdf
+</script>
 <!-- <Svelvet
   nodes={initialNodes}
   edges={initialEdges}
@@ -217,8 +423,8 @@
   height={900}
   background
 /> -->
-<div on:dragenter={handleDragEnter} on:dragleave={handleDragLeave}>
-  <Svelvet nodes={initialNodes} edges={initialEdges} />
+<div id="drop_zone" on:dragenter={handleDragEnter} on:dragleave={handleDragLeave} on:drop={handleDragDrop}>
+  <Svelvet nodes={initialNodes} edges={initialEdges} canvasId={"bob"} />
 </div>
 <h2 id="app_status">Drag status: {status}</h2>
 <h1>Drop Zone</h1>
@@ -226,9 +432,7 @@
 <div
   on:dragenter={handleDragEnter}
   on:dragleave={handleDragLeave}
-  on:drop={handleDragDrop}
   bind:this={drop_zone}
-  id="drop_zone"
   ondragover="return false"
 >
   {#each objects.filter((v) => dropped.includes(`${v.id}`)) as { id }, i}
@@ -264,7 +468,7 @@
     -webkit-user-select: none;
   }
 
-  #drop_zone {
+  #drop_zone_ex {
     background-color: #eee;
     border: #999 1px solid;
     width: 280px;
