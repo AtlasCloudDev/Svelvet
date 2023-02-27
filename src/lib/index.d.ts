b26c697ace66267631dcd3d6d0df1f80734c129f
jakegibson <jake@j3.io>
2023-02-27 15:39:11 -0700

    add store/user api exports

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -2,3 +2,9 @@ declare module 'svelvet';
 import Svelvet from './container/views/Svelvet.svelte';
 export default Svelvet;
 export * from './types/types';
+
+// Export Store and User API and Types
+export * from './store/controllers/storeApi';
+export * from './store/controllers/userApi';
+export * from './store/controllers/util';
+export * from './store/types/types';
